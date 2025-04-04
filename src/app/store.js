bb0f7caffdb06eb87229061da447ab7693e098de
tejasderle24 <tejasderle24@gmail.com>
2025-04-04 14:11:18 +0530

    creating paste Reducure

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import pasteReducer from '../redux/pasteSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    paste: pasteReducer,
+  },
 })

556c259a2f8698ee9dce8e45ec65b52b8ab1eda1
tejasderle24 <tejasderle24@gmail.com>
2025-04-04 16:26:18 +0530

    Add react-hot-toast Dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import App from './App'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <Toaster/>
     </Provider>,
   </StrictMode>,
 )

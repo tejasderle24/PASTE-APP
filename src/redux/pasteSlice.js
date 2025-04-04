bac263dc27d4b6b5aae3a758ed036086006ed5d7
tejasderle24 <tejasderle24@gmail.com>
2025-04-04 16:25:52 +0530

    Build the all slice funcality

--- src/redux/pasteSlice.js
+++ src/redux/pasteSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
+import toast from 'react-hot-toast';
 
 const initialState = {
-  paste: localStorage.getItem('paste') 
-  ? JSON.parse(localStorage.getItem('paste')) : []
+  paste: localStorage.getItem('pastes')
+    ? JSON.parse(localStorage.getItem('pastes')) : []
 }
 
 export const pasteSlice = createSlice({
@@ -10,18 +11,43 @@ export const pasteSlice = createSlice({
   initialState,
   reducers: {
     addToPaste: (state, action) => {
-      
+      const paste = action.payload;
+      const existingPaste = state.paste.find((item) => item.id === paste.id);
+      if (existingPaste) {
+        toast.error("Paste already exists")
+        return;
+      }
+      state.paste.push(paste)
+      localStorage.setItem('pastes', JSON.stringify(state.paste));
+      toast.success("Paste Added Successfully")
+
     },
     removeFormPaste: (state, action) => {
-      
+      const paste = action.payload;
+      const index = state.paste.findIndex((item) => item.id === paste.id);
+      if (index !== -1) {
+
+        state.paste.solice(index, 1);
+        localStorage.setItem('pastes', JSON.stringify(state.paste));
+        toast.success("Paste Remove Successfully")
+      }
     },
     updateToPaste: (state, action) => {
-      
+      const paste = action.payload;
+      const index = state.paste.findIndex((item) => item.id === paste.id);
+      if (index !== -1) {
+        state.paste[index] = paste;
+        localStorage.setItem('pastes', JSON.stringify(state.paste));
+        toast.success("Paste updated Successfully")
+      } else {
+        toast.error("Paste not found")
+      }
     },
     resetAllPaste: (state, action) => {
-      
+      state.paste = [];
+      localStorage.removeItem('paste')
     },
-    
+
   },
 })
 
